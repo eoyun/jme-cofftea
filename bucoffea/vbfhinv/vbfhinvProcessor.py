36b29be280431f27003e4b1e9145196d0cb31dfe
Alp Akpinar <aakpinar@bu.edu>
2021-09-14 18:40:57 -0500

    Fixup

--- bucoffea/vbfhinv/vbfhinvProcessor.py
+++ bucoffea/vbfhinv/vbfhinvProcessor.py
@@ -948,7 +948,7 @@ class vbfhinvProcessor(processor.ProcessorABC):
             ezfill('vecb',        vecb=vec_b[mask],            weight=rweight[mask] )
             ezfill('dphitkpf',    dphi=dphitkpf[mask],         weight=rweight[mask] )
 
-            if re.match('sr_vbf.*', region):
+            if re.match('sr_vbf.*', region) or region in ['cr_vbf_qcd']:
                 ezfill('dphitkpf_ak4_eta0',     dphi=dphitkpf[mask],     jeteta=diak4.i0.abseta[mask].flatten(),     weight=rweight[mask])
 
             # Consider events where only (at least) one of the jets is in horn
