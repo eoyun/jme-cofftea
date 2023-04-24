94b0fb10fb81aa9e5ebb3bc83e07fe53f4acb39e
Alp <aakpinar@bu.edu>
2023-04-24 21:54:43 +0200

    Update jmecofftea script paths

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
     packages = find_packages(),    
     install_requires = requirements,
     scripts=[
-        'bucoffea/execute/buexec',
-        'bucoffea/execute/bumon',
-        'bucoffea/scripts/bumerge'
+        'jmecofftea/execute/buexec',
+        'jmecofftea/execute/bumon',
+        'jmecofftea/scripts/bumerge'
         ],
 )
